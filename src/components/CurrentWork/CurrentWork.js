58f5539da888f6e7434b90998dbc97a35587d11b
hampster2018 <shieldse01@yahoo.com>
2022-11-06 18:12:03 -0600

    enviorment API key and axios get function

--- src/components/CurrentWork/CurrentWork.js
+++ src/components/CurrentWork/CurrentWork.js
@@ -1,11 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { useEffect, useState } from "react"
+import axios from "axios";
 import WorkCard from "./WorkCard";
+
 
 import "./CurrentWork.css"
 
 export default function CurrentWork () {
 
     const [works, setWorks] = useState(["Hi", "Billy", "Bob"])
+
+    const header = {
+        'auth': process.env.REACT_APP_GITHUB_AUTH,
+    }
+
+    axios.get('https://api.github.com/repos/hampster2018/SoftwareEngineeringProject', header)
+        .then((response) => console.log(response.data));
 
     const Works = works.map((work, index) => 
         <WorkCard text={work} key={index}/>
