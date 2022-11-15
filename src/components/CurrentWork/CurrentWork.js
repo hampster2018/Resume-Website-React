58f31c7c0bab44baef9e60d681cdc6023bb3fd71
Eric Shields <shieldse01@yahoo.com>
2022-11-15 10:27:41 -0600

    finished fetch for current work

--- src/components/CurrentWork/CurrentWork.js
+++ src/components/CurrentWork/CurrentWork.js
@@ -8,22 +8,54 @@ import "./CurrentWork.css"
 
 export default function CurrentWork () {
 
-    const [works, setWorks] = useState(["Hi", "Billy", "Bob"])
+    const [works, setWorks] = useState(null)
+    const [repos, setRepos] = useState([]);
+    const [loaded, setLoaded] = useState(false);
 
-    const header = {
+    const params = {
         'auth': process.env.REACT_APP_GITHUB_AUTH,
+    };
+
+    async function fetchRepos() {
+        await axios.get('https://api.github.com/users/hampster2018/repos?sort=updated', {params: params})
+            .then((response) => {
+                console.log(response)
+                setRepos(response.data);
+                setLoaded(true);    
+            })
     }
 
-    axios.get('https://api.github.com/repos/hampster2018/SoftwareEngineeringProject', header)
-        .then((response) => console.log(response.data));
+    async function fetchLanguages() {
 
-    const Works = works.map((work, index) => 
-        <WorkCard text={work} key={index}/>
-    )
+    }
+
+    function makeWorkCards () {
+        console.log()
+        setWorks(repos.map((repos, index) =>
+            <WorkCard text={repos.name} key={index} data={repos}/>
+        ))
+    }
+
+    useEffect(() => {
+        console.log("running")
+        if (!loaded) {
+            fetchRepos();
+        }
+        makeWorkCards();
+
+    }, [loaded]);
+
+    if (!loaded) {
+        return (
+            <div className="Grid-Container">
+                Loading...
+            </div>
+        )
+    }
 
     return (
         <div className="Grid-Container">
-            {Works}
+            {works}
         </div>
     );
 
