0568632ded9e2071198970e05d655577b02653ed
hampster2018 <shieldse01@yahoo.com>
2023-01-17 09:34:21 -0600

    maybe broken

--- src/components/CurrentWork/CurrentWork.js
+++ src/components/CurrentWork/CurrentWork.js
@@ -3,6 +3,12 @@ import { useEffect, useState } from "react"
 import axios from "axios";
 import WorkCard from "./WorkCard";
 
+function sortTimes (objects) {
+    return objects.sort((a, b) => {
+        console.log(a)
+        return JSON.parse(a).updated_at - JSON.parse(b).updated_at;
+    });
+};
 
 import "./CurrentWork.css"
 
@@ -14,8 +20,11 @@ export default function CurrentWork () {
         'auth': process.env.REACT_APP_GITHUB_AUTH,
     }
 
-    axios.get('https://api.github.com/repos/hampster2018/SoftwareEngineeringProject', header)
-        .then((response) => console.log(response.data));
+    axios.get('https://api.github.com/users/hampster2018/repos', header)
+        .then((response) => {
+            const array = sortTimes(response.data);
+            console.log(array)
+        });
 
     const Works = works.map((work, index) => 
         <WorkCard text={work} key={index}/>
